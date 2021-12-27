d3e204b5922e0ba1d970526d3930df3553674e4c
Manaball123 <57663038+Manaball123@users.noreply.github.com>
2021-12-27 20:42:50 +0800

    EVERYTHING IS READY BAHAHAHAHHAHAHAHAHHAHAHAHAHAHAHAHHAHAHAhA

--- perlin_thing/lib.py
+++ perlin_thing/lib.py
@@ -3,8 +3,18 @@
 #interpolation to make it smooth
 import random
 import numpy as np
+from numpy.lib.function_base import average
 import Vector2 
 import copy
+
+
+
+def clamp(value, min, max):
+    if(value > max):
+        return max
+    elif(value < min):
+        return min
+    return value
 
 class waveMatrix:
 
@@ -17,10 +27,12 @@ class waveMatrix:
         self.matrixCache = np.array([[0.0] * (yGrids * gridY)] * (xGrids * gridX))
         self.min = min
         self.max = max
+        self.valueRange = self.max - self.min
         self.vecMag = vecMag
         self.gridX = gridX
         self.gridY = gridY
         self.maxMag = vecMag + Vector2.VectorLength(np.array([(gridX/2), (gridY/2)]))
+        self.poses = np.array([np.array([1,0]), np.array([-1,0]), np.array([0,1]), np.array([0,-1])])
         #print(self.maxMag)
 
     def GetDirection(self,value):
@@ -29,51 +41,88 @@ class waveMatrix:
         else:
             return 1
 
-    def GenerateGrid(self,startX,startY):
-        gridMatrix = np.array([[0.0] * self.gridY] * self.gridX)
-        vectorX = random.uniform(-self.vecMag, self.vecMag)
-        vectorY = Vector2.GetVectorFromLength(self.vecMag, vectorX) * self.GetDirection(random.randint(0,1))
-        
-        vector = np.array([vectorX,vectorY])
-        #print(vector)
-        for i in range(len(gridMatrix)):
-            for j in range(len(gridMatrix[i])):
-                #print("x is " + str(i - (self.gridX/2)) + ", y is " + str(j - (self.gridY/2)) + ", distance is " + str(Vector2.VectorDist(np.array([i - (self.gridX/2), j - (self.gridY/2)]), vector)))
-                #gridMatrix[i][j] = 1 - (Vector2.VectorDist(np.array([i - (self.gridX/2), j - (self.gridY/2)]), vector)/self.maxMag)
-                self.matrix[startX + i][startY + j] = 1 - (Vector2.VectorDist(np.array([i - (self.gridX/2), j - (self.gridY/2)]), vector)/self.maxMag)
-    
     def GenerateMatrix(self):
-        for i in range(self.xGrids - 1):
-            for j in range(self.yGrids - 1):
-                self.GenerateGrid(i * self.gridX, j * self.gridY)
+        for i in range(self.xGrids):
+            for j in range(self.yGrids):
+                gridMatrix = np.array([[0.0] * self.gridY] * self.gridX)
+                vectorX = random.uniform(-self.vecMag, self.vecMag)
+                #vectorY = Vector2.GetVectorFromLength(self.vecMag, vectorX) * self.GetDirection(random.randint(0,1))
+                vectorY=  random.uniform(-self.vecMag, self.vecMag)
+                vector = np.array([vectorX,vectorY])
+                for k in range(len(gridMatrix)):
+                    for h in range(len(gridMatrix[k])):
+                        #breaking it down to avoid confusion
+                        #gets distance from vector
+                        vecDist = Vector2.VectorDist(np.array([k - (self.gridX/2), h - (self.gridY/2)]), vector)
+                        
 
-    #range is not used rn, cba
-    def InterpolateMatrix(self,interpRange):
-        self.matrixCache = copy.deepcopy(self.matrix)
-        
+                        self.matrix[i * self.gridX + k][j * self.gridY + h] = ((1 - (vecDist/self.maxMag)) * self.valueRange) + self.min
+
+    def InterpolateMatrix(self, times, minDelta, interpRange, sampleSize):
+        for a in range(times):
+            self.matrixCache = copy.deepcopy(self.matrix)
+            print(a)
+            for i in range(len(self.matrix)):
+                for j in range(len(self.matrix[i])):
+                    
+                    """
+                    avgSum = self.matrix[i][j]
+                    samples = 1
+                    for k in range(4):
+                        xPos = i + self.poses[k][0]
+                        yPos = j + self.poses[k][1]
+                        if(xPos < len(self.matrix) and xPos >= 0 and yPos < len(self.matrix[i]) and yPos >= 0):
+
+                            
+                            if(abs(self.matrixCache[i][j] - self.matrixCache[xPos, yPos]) >= minDelta):
+                                average = (self.matrixCache[i][j] + self.matrixCache[xPos, yPos])/2
+                                avgSum += average
+                                samples += 1
+                                self.matrix[xPos, yPos] = average
+                            
+                    
+                    self.matrix[i][j] = avgSum/samples
+                                #print("changed element at " + str(i) + ", " + str(j) + " and " + str(xPos) +", " + str(yPos) + " to" + str(average))
+                            
+                            
+                            
+                    
+                    while elements < sampleSize:
+                        xOffset = random.randint(-interpRange,interpRange)
+                        yOffset = random.randint(-interpRange,interpRange)
+                        xPos = i + xOffset
+                        yPos = j + yOffset
+                        if(xPos < len(self.matrix) and xPos >= 0 and yPos < len(self.matrix[i]) and yPos >= 0):
+                            sum += self.matrixCache[xPos][yPos]
+                            elements += 1
+                    """
+                    sum = self.matrixCache[i][j]
+                    elements = 1
+                    if(i - 1 >= 0):
+                        sum += self.matrixCache[i - 1][j]
+                        elements += 1
+
+                    if(i + 1 < len(self.matrix)):
+                        sum += self.matrixCache[i + 1][j]
+                        elements += 1
+
+                    if(j - 1 >= 0):
+                        sum += self.matrixCache[i][j - 1]
+                        elements += 1
+
+                    if(j + 1 < len(self.matrix[i])):
+                        sum += self.matrixCache[i][j + 1]
+                        elements += 1
+                        
+                    
+
+                    self.matrix[i][j] = sum/elements
+                    
+            
+    def AddMatrix(self, newMatrix, base):
         for i in range(len(self.matrix)):
             for j in range(len(self.matrix[i])):
-                sum = self.matrixCache[i][j]
-                elements = 1
-                if(i - 1 >= 0):
-                    sum += self.matrixCache[i - 1][j]
-                    elements += 1
-
-                if(i + 1 < len(self.matrix)):
-                    sum += self.matrixCache[i + 1][j]
-                    elements += 1
-
-                if(j - 1 >= 0):
-                    sum += self.matrixCache[i][j - 1]
-                    elements += 1
-
-                if(j + 1 < len(self.matrix[i])):
-                    sum += self.matrixCache[i][j + 1]
-                    elements += 1
-                
-                self.matrix[i][j] = sum/elements
-                
-            
+                self.matrix[i][j] = clamp(self.matrix[i][j] + newMatrix[i][j] - base, self.min, self.max)
     
         
 
--- perlin_thing/main.py
+++ perlin_thing/main.py
@@ -3,34 +3,71 @@ from lib import waveMatrix
 
 
 
-matrix1 = waveMatrix(8,8,0,1,8,16,16)
+matrix1 = waveMatrix(2,2,0,10,32,64,64)
+matrix2 = waveMatrix(4,4,1,9,16,32,32)
+matrix3 = waveMatrix(8,8,1,9,8,16,16)
+matrix4 = waveMatrix(16,16,2,8,4,8,8)
+matrix5 = waveMatrix(32,32,3,7,2,4,4)
+
 
 matrix1.GenerateMatrix()
+matrix2.GenerateMatrix()
+matrix3.GenerateMatrix()
+matrix4.GenerateMatrix()
+matrix5.GenerateMatrix()
 
 depth = ["  ", " .", " -", " *", " +", " M", " 0", "[[", "##", "@@", "██"]
 
 
 def GetDepth(value):
-    roundedVal = int(np.round(value,1)*10)
+    roundedVal = int(np.round(value,1))
     return depth[roundedVal]
     
-string = ""
-for i in range(len(matrix1.matrix)):
-    for j in range(len(matrix1.matrix[i])):
-        string += GetDepth(matrix1.matrix[i][j]) + ""
-    string += "\n"
-print(string)
-for i in range(25):
-    matrix1.InterpolateMatrix(1)
+def printMatrix(matrix):
+    string = ""
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            string += GetDepth(matrix[i][j]) + ""
+        string += "\n"
+    print(string)
+
+inter1 = 25
+inter2 = 25
+def main1():
+    matrix1.InterpolateMatrix(inter1,2,1,2)
+    matrix2.InterpolateMatrix(inter1,2,1,2)
+    matrix3.InterpolateMatrix(inter1,2,1,2)
+    matrix4.InterpolateMatrix(inter1,2,1,2)
+    matrix5.InterpolateMatrix(inter1,2,1,2)
+
+    printMatrix(matrix1.matrix)
+
+    matrix1.AddMatrix(matrix2.matrix,5)
+    matrix1.InterpolateMatrix(inter2,2,1,2)
+    printMatrix(matrix1.matrix)
 
 
-string = ""
-for i in range(len(matrix1.matrix)):
-    for j in range(len(matrix1.matrix[i])):
-        string += GetDepth(matrix1.matrix[i][j]) + ""
-    string += "\n"
-print(string)
+    matrix1.AddMatrix(matrix3.matrix,5)
+    matrix1.InterpolateMatrix(inter2,2,1,2)
+    printMatrix(matrix1.matrix)
 
+    matrix1.AddMatrix(matrix4.matrix,5)
+    matrix1.InterpolateMatrix(inter2,2,1,2)
+    printMatrix(matrix1.matrix)
+
+    matrix1.AddMatrix(matrix5.matrix,5)
+    matrix1.InterpolateMatrix(inter2,2,1,2)
+    printMatrix(matrix1.matrix)
+
+    matrix1.InterpolateMatrix(50,2,1,2)
+    printMatrix(matrix1.matrix)
+
+def main2():
+    printMatrix(matrix2.matrix)
+    matrix2.InterpolateMatrix(200,0.05,1,1)
+    printMatrix(matrix2.matrix)
+
+main1()
 input()
 
 
